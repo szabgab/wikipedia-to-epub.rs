7d1bb2ca97f7c8e25fb0cfa4c9ba8f041f1e24d4
Gabor Szabo <gabor@szabgab.com>
2026-06-12 14:30:23 +0300

    regenerate all

--- regenerate.sh
+++ regenerate.sh
@@ -1,22 +1,35 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -e
+
+function regenerate() {
+    echo $filename;
+    rm -f demo.epub
+    cargo run -- --output demo.epub --local pages/ $filename
+
+    name=$(basename $filename | cut -f1 -d'.')
+    echo $name
+    cd expected/$name
+    unzip -o ../../demo.epub
+    cd -
+}
 
 if [ "$1" == "" ]; then
-    echo Usage: $0 examples/planets.yaml
+    echo "Usage: $0 [all|examples/planets.yaml]"
     exit 1
 fi
 
-if [ ! -f "$1" ]; then
-    echo File $1 does not exist.
-    exit 2
+
+if [ "$1" == "all" ]; then
+    for filename in examples/*.yaml;
+        do regenerate
+    done
+else
+    if [ ! -f "$1" ]; then
+        echo File $1 does not exist.
+        exit 2
+    fi
+    filename=$1
+    regenerate
 fi
 
-echo $1
 
-cargo run -- --output demo.epub --local pages/  $1
-
-name=$(basename $1 | cut -f1 -d'.')
-#echo $name
-cd expected/$name
-unzip -o ../../demo.epub
-cd -
 
